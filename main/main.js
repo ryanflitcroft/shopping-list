3e9696d9e01490899309bc28e2d4e198714e0898
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-01-06 16:35:39 -0800

    readme.md plan, initial html js setup

--- main/main.js
+++ main/main.js
@@ -1,9 +1,29 @@
+/* eslint-disable no-console */
 import { checkAuth, logout } from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const listForm = document.querySelector('#list-form');
+const itemList = document.querySelector('#item-list');
+const deleteButton = document.querySelector('#delete-button');
+
+console.log(listForm, itemList, deleteButton);
+
+window.addEventListener('load', () => {
+
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+listForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    listForm.reset();
+});
+
+deleteButton.addEventListener('click', () => {
+
 });
